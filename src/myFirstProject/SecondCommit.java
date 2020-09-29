7acec7856e27d75887ea674f4ca67ebd234f9c6b
rafa rafa <aymen.nadal@yahoo.fr>
2020-09-29 22:35:21 +0100

    Hi there

--- src/myFirstProject/SecondCommit.java
+++ src/myFirstProject/SecondCommit.java
@@ -3,5 +3,6 @@ package myFirstProject;
 public class SecondCommit {
 	public static void main(String[] args) {
 		System.out.println("Things get interesting :D");
+		System.out.println("So ! what now?");
 	}
 }
