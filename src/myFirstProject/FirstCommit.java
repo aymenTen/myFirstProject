e3e2aaddbbb7cc07872c9b0768d16d5e709dad39
aymen <aymen@DESKTOP-CG92HRI>
2020-09-29 21:39:53 +0100

    ssss

---
+++ src/myFirstProject/FirstCommit.java
@@ -0,0 +1,7 @@
+package myFirstProject;
+
+public class FirstCommit {
+	public static void main(String[] args) {
+		System.out.println("Hello world ! this is my first commitment in this account");
+	}
+}
